6d2b8176a221d981c8240b2ea842d07c5a49d3fd
Okwudili David <michealdavid910@gmail.com>
2020-05-25 03:02:36 +0100

    added: version

--- mod.ts
+++ mod.ts
@@ -1,8 +1,12 @@
 // validators
 import isAlpha, { locales as isAlphaLocales } from "./lib/isAlpha.ts";
 
+// validator version
+const version = "0.0.1";
+
 const validator: any = {
   isAlpha,
+  isAlphaLocales
 };
 
 export default validator;
